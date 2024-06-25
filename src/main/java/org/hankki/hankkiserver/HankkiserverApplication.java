1f6801e8df3d43fb6003194cd460b5ba29573db8
Parkjyun <jaeyun0507@naver.com>
2024-06-25 15:58:16 +0900

    [init] initial settings

---
+++ src/main/java/org/hankki/hankkiserver/HankkiserverApplication.java
@@ -0,0 +1,13 @@
+package org.hankki.hankkiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HankkiserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HankkiserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hankki/hankkiserver/HankkiserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hankki.hankkiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HankkiserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

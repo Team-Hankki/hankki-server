2392bd6155a8eb3dcddca728df5f2ce59ff8069c
kgy1008 <gayeon1008@g.skku.edu>
2024-07-01 22:36:03 +0900

    [feat] create custom annotation for UserId

--- src/main/java/org/hankki/hankkiserver/HankkiserverApplication.java
+++ src/main/java/org/hankki/hankkiserver/HankkiserverApplication.java
@@ -2,8 +2,10 @@ package org.hankki.hankkiserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HankkiserverApplication {
 
 	public static void main(String[] args) {

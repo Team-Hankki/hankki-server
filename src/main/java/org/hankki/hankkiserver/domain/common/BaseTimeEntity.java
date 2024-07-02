d4a4d779ae02ddf121a46ac9f3f0e286dbd6b427
Parkjyun <jaeyun0507@naver.com>
2024-07-02 15:47:57 +0900

    [feat] create basetimeentity for domain

---
+++ src/main/java/org/hankki/hankkiserver/domain/common/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package org.hankki.hankkiserver.domain.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
---
+++ src/main/java/org/hankki/hankkiserver/domain/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package org.hankki.hankkiserver.domain.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}

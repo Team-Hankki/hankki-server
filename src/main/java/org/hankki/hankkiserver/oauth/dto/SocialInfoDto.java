d6913539656b0d8cc6c4856284c707c62b4598b7
kgy1008 <gayeon1008@g.skku.edu>
2024-07-03 19:32:48 +0900

    [fix] add final keyword

--- src/main/java/org/hankki/hankkiserver/oauth/dto/SocialInfoDto.java
+++ src/main/java/org/hankki/hankkiserver/oauth/dto/SocialInfoDto.java
@@ -4,7 +4,10 @@ public record SocialInfoDto(
         String serialId,
         String name,
         String email) {
-    public static SocialInfoDto of(String serialId, String name, String email) {
+    public static SocialInfoDto of(
+            final String serialId,
+            final String name,
+            final String email) {
         return new SocialInfoDto(serialId, name, email);
     }
 }

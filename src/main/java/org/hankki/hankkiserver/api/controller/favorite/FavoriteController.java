40b0c2d4fc823cd29a9761c3dcbe1160c1e73c8f
PicturePark1101 <seojin5565@gmail.com>
2024-07-08 09:21:58 +0900

    [refactor] refactor createFavorite return format

--- src/main/java/org/hankki/hankkiserver/api/controller/favorite/FavoriteController.java
+++ src/main/java/org/hankki/hankkiserver/api/controller/favorite/FavoriteController.java
@@ -8,6 +8,7 @@ import org.hankki.hankkiserver.api.service.favorite.command.FavoritePostCommand;
 import org.hankki.hankkiserver.api.controller.favorite.request.FavoritePostRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +24,9 @@ public class FavoriteController {
   @PostMapping
   public ResponseEntity<?> createFavorite(@RequestBody @Valid FavoritePostRequest favoritePostRequest) {
 
-    return ResponseEntity.created(
-        URI.create(favoriteCommandService.create(
-            FavoritePostCommand.of(1L, favoritePostRequest)).toString())
+     return ResponseEntity.status(HttpStatus.CREATED)
+        .header("Location", favoriteCommandService.create(FavoritePostCommand.of(1L, favoritePostRequest)).toString()
     ).build();
+
   }
 }

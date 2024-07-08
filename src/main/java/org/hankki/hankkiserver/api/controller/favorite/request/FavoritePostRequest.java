22bbe3b5e891adf3888c503a3efb4e40084393e1
PicturePark1101 <seojin5565@gmail.com>
2024-07-08 09:08:19 +0900

    [refactor] change dto name

--- src/main/java/org/hankki/hankkiserver/api/controller/favorite/request/FavoritePostRequest.java
+++ src/main/java/org/hankki/hankkiserver/api/controller/favorite/request/FavoritePostRequest.java
@@ -1,15 +1,13 @@
-package org.hankki.hankkiserver.domain.favorite.service.dto.request;
+package org.hankki.hankkiserver.api.controller.favorite.request;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import java.util.List;
 
-public record FavoritePostDto (
+public record FavoritePostRequest(
     @NotEmpty(message = "제목이 비었습니다.")
     @Size(max = 20, message = "제목 size가 20 이상입니다.")
     String title,
-
     List<String> details // 나중에 커스텀 어노테이션 적용해서 형식 검증예정
 ) {
 }

45dbac48f842e572d66ce0177f522698540d108b
PicturePark1101 <seojin5565@gmail.com>
2024-07-08 15:33:12 +0900

    [fix] fix import error occurring after a rebase

--- src/main/java/org/hankki/hankkiserver/api/favorite/service/FavoriteAdpater.java
+++ src/main/java/org/hankki/hankkiserver/api/favorite/service/FavoriteAdpater.java
@@ -1,10 +1,10 @@
 package org.hankki.hankkiserver.api.favorite.service;
 
 import lombok.RequiredArgsConstructor;
-import org.hankki.hankkiserver.common.dto.ErrorMessage;
+import org.hankki.hankkiserver.common.code.ErrorCode;
+import org.hankki.hankkiserver.common.exception.EntityNotFoundException;
 import org.hankki.hankkiserver.domain.favorite.model.Favorite;
 import org.hankki.hankkiserver.domain.favorite.repository.FavoriteRepository;
-import org.hankki.hankkiserver.exception.EntityNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,7 +15,7 @@ public class FavoriteAdpater {
 
   public Favorite findById(final long id) {
 
-    return favoriteRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(ErrorMessage.FAVORITE_NOT_FOUND));
+    return favoriteRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(ErrorCode.FAVORITE_NOT_FOUND));
 
   }
 }

147b3cc73fa916c3cdf09baca48b648ff90d304e
PicturePark1101 <seojin5565@gmail.com>
2024-07-10 03:56:52 +0900

    [feat] add delete query

--- src/main/java/org/hankki/hankkiserver/domain/favorite/repository/FavoriteRepository.java
+++ src/main/java/org/hankki/hankkiserver/domain/favorite/repository/FavoriteRepository.java
@@ -1,7 +1,16 @@
 package org.hankki.hankkiserver.domain.favorite.repository;
 
+import java.util.List;
 import org.hankki.hankkiserver.domain.favorite.model.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+
+  @Modifying
+  @Query("delete from Favorite f where f in :favorites")
+  void deleteAll(@Param("favorites") List<Favorite> favorites);
 }
--- src/main/java/org/hankki/hankkiserver/domain/favoritestore/repository/FavoriteStoreRepository.java
+++ src/main/java/org/hankki/hankkiserver/domain/favoritestore/repository/FavoriteStoreRepository.java
@@ -1,7 +1,16 @@
 package org.hankki.hankkiserver.domain.favoritestore.repository;
 
+import java.util.List;
+import org.hankki.hankkiserver.domain.favorite.model.Favorite;
 import org.hankki.hankkiserver.domain.favoritestore.model.FavoriteStore;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FavoriteStoreRepository extends JpaRepository<FavoriteStore, Long> {
+
+  @Modifying
+  @Query("delete from FavoriteStore fs where fs.favorite in :favorites")
+  void deleteAllByFavorites(@Param("favorites")List<Favorite> favorites);
 }

c838a7ab9fa22815bba3e6502dcf7e26766c62b6
PicturePark1101 <seojin5565@gmail.com>
2024-07-10 04:00:37 +0900

    [refactor] refactor response, request dto

--- src/main/java/org/hankki/hankkiserver/api/favorite/controller/request/FavoriteDeleteRequest.java
+++ src/main/java/org/hankki/hankkiserver/api/favorite/controller/request/FavoriteDeleteRequest.java
@@ -1,11 +1,8 @@
 package org.hankki.hankkiserver.api.favorite.controller.request;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 public record FavoriteDeleteRequest(
-    @NotEmpty
-    List<@NotNull Long> favoriteIds
+    List<Long> favoriteIds
 ) {
 }
--- src/main/java/org/hankki/hankkiserver/api/favorite/controller/request/FavoritePostRequest.java
+++ src/main/java/org/hankki/hankkiserver/api/favorite/controller/request/FavoritePostRequest.java
@@ -1,13 +1,12 @@
 package org.hankki.hankkiserver.api.favorite.controller.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record FavoritePostRequest(
     @NotBlank(message = "제목이 비었습니다.")
-    @Size(max = 20, message = "제목 size가 20 이상입니다.")
+    @Size(max = 20, message = "제목 길이가 20 이상입니다.")
     String title,
     List<String> details
 ) {

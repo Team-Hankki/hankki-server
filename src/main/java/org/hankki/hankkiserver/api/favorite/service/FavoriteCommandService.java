98460dc4be5b29788d372db21a4be8982ea766b1
PicturePark1101 <seojin5565@gmail.com>
2024-07-09 22:29:57 +0900

    [feat] feat delete FavoriteStore

--- src/main/java/org/hankki/hankkiserver/api/favorite/service/FavoriteCommandService.java
+++ src/main/java/org/hankki/hankkiserver/api/favorite/service/FavoriteCommandService.java
@@ -2,13 +2,16 @@ package org.hankki.hankkiserver.api.favorite.service;
 
 import jakarta.transaction.Transactional;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.hankki.hankkiserver.api.favorite.controller.request.FavoriteDeleteRequest;
+import org.hankki.hankkiserver.api.favoritestore.service.FavoriteStoreDeleter;
 import org.hankki.hankkiserver.common.code.ErrorCode;
 import org.hankki.hankkiserver.common.exception.OperationFailedException;
 import org.hankki.hankkiserver.domain.favorite.model.Favorite;
 import org.hankki.hankkiserver.domain.favorite.repository.FavoriteRepository;
 import org.hankki.hankkiserver.api.favorite.service.command.FavoritePostCommand;
+import org.hankki.hankkiserver.domain.favoritestore.model.FavoriteStore;
 import org.hankki.hankkiserver.domain.user.model.User;
 import org.hankki.hankkiserver.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -20,6 +23,7 @@ public class FavoriteCommandService {
   private final FavoriteRepository favoriteRepository;
   private final UserRepository userRepository;
   private final FavoriteFinder favoriteFinder;
+  private final FavoriteStoreDeleter favoriteStoreDeleter;
 
   @Transactional
   public Long create(final FavoritePostCommand favoritePostCommand) {
@@ -43,11 +47,17 @@ public class FavoriteCommandService {
     List<Long> favoriteIds = favoriteDeleteRequest.favoriteIds();
 
     List<Favorite> favorites = favoriteIds.stream()
-        .map(favoriteFinder::findById)  // 각 ID에 대해 findById 호출
+        .map(favoriteFinder::findById)
         .toList();
+
+    // 관련된 favoriteStores batch 설정해서 알아서 가져올 듯
+    List<Long> favoriteStores =  favorites.stream()
+        .flatMap(favorite -> favorite.getFavoriteStores().stream())
+        .map(FavoriteStore::getId).toList();
 
     if (!favorites.isEmpty()) {
       favoriteRepository.deleteAll(favorites);
+      favoriteStoreDeleter.deleteByIds(favoriteStores);
     } else {
     throw new OperationFailedException(ErrorCode.INTERNAL_SERVER_ERROR);
   }

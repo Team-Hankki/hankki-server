0298f2d2eb8eb8ee85f10eeed691e8462d5c1195
Parkjyun <jaeyun0507@naver.com>
2024-07-09 11:16:58 +0900

    [refac] fix typo in UserErrorCode.java

--- src/main/java/org/hankki/hankkiserver/common/code/UserErrorCode.java
+++ src/main/java/org/hankki/hankkiserver/common/code/UserErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum UserErrorCode implements ErrorCode {
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "등록되지 않은 회원입니다."),
-    USER_INFO_NOT_FOUND(HttpStatus.NOT_FOUND, "회원 정보를 찾을 수 없습니다."),;
+    USER_INFO_NOT_FOUND(HttpStatus.NOT_FOUND, "회원 정보를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;

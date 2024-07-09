9484aaca63cc5b2e2a61fa573c7e060a6fc938c7
PicturePark1101 <seojin5565@gmail.com>
2024-07-10 04:00:52 +0900

    [feat] add delete and set response

--- src/main/java/org/hankki/hankkiserver/api/favorite/controller/FavoriteController.java
+++ src/main/java/org/hankki/hankkiserver/api/favorite/controller/FavoriteController.java
@@ -2,16 +2,13 @@ package org.hankki.hankkiserver.api.favorite.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.hankki.hankkiserver.api.dto.BaseResponse;
+import org.hankki.hankkiserver.api.dto.HankkiResponse;
 import org.hankki.hankkiserver.api.favorite.controller.request.FavoriteDeleteRequest;
 import org.hankki.hankkiserver.api.favorite.service.FavoriteCommandService;
 import org.hankki.hankkiserver.api.favorite.service.command.FavoritePostCommand;
 import org.hankki.hankkiserver.api.favorite.controller.request.FavoritePostRequest;
 import org.hankki.hankkiserver.auth.UserId;
-import org.hankki.hankkiserver.common.code.SuccessCode;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.hankki.hankkiserver.common.code.CommonSuccessCode;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,24 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/favorites")
+@RequestMapping("/api/v1")
 public class FavoriteController {
 
   private final FavoriteCommandService favoriteCommandService;
 
-  @PostMapping
-  public ResponseEntity<?> createFavorite(@UserId final Long userId, @RequestBody @Valid final FavoritePostRequest favoritePostRequest) {
+  @PostMapping("/favorites")
+  public HankkiResponse<Void> createFavorite(@UserId final Long userId, @RequestBody @Valid final FavoritePostRequest favoritePostRequest) {
 
-    return ResponseEntity.status(HttpStatus.CREATED)
-        .header("Location", favoriteCommandService.create(FavoritePostCommand.of(userId, favoritePostRequest)).toString()).build();
-
+    favoriteCommandService.create(FavoritePostCommand.of(userId, favoritePostRequest));
+    return HankkiResponse.success(CommonSuccessCode.CREATED);
   }
 
-  @DeleteMapping
-  public ResponseEntity<?> deleteFavorite(@RequestBody @Valid final FavoriteDeleteRequest favoriteDeleteRequest) {
+  @PostMapping("/favorites/delete")
+  public HankkiResponse<Void> deleteFavorite(@RequestBody final FavoriteDeleteRequest favoriteDeleteRequest) {
 
-    favoriteCommandService.delete(favoriteDeleteRequest);
-    return ResponseEntity.noContent().build();
-
+    favoriteCommandService.deleteFavorites(favoriteDeleteRequest);
+    return HankkiResponse.success(CommonSuccessCode.NO_CONTENT);
   }
 }
--- src/main/java/org/hankki/hankkiserver/api/favorite/service/FavoriteCommandService.java
+++ src/main/java/org/hankki/hankkiserver/api/favorite/service/FavoriteCommandService.java
@@ -2,18 +2,14 @@ package org.hankki.hankkiserver.api.favorite.service;
 
 import jakarta.transaction.Transactional;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.hankki.hankkiserver.api.auth.service.UserFinder;
 import org.hankki.hankkiserver.api.favorite.controller.request.FavoriteDeleteRequest;
 import org.hankki.hankkiserver.api.favoritestore.service.FavoriteStoreDeleter;
-import org.hankki.hankkiserver.common.code.ErrorCode;
-import org.hankki.hankkiserver.common.exception.OperationFailedException;
 import org.hankki.hankkiserver.domain.favorite.model.Favorite;
 import org.hankki.hankkiserver.domain.favorite.repository.FavoriteRepository;
 import org.hankki.hankkiserver.api.favorite.service.command.FavoritePostCommand;
-import org.hankki.hankkiserver.domain.favoritestore.model.FavoriteStore;
 import org.hankki.hankkiserver.domain.user.model.User;
-import org.hankki.hankkiserver.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,45 +17,32 @@ import org.springframework.stereotype.Service;
 public class FavoriteCommandService {
 
   private final FavoriteRepository favoriteRepository;
-  private final UserRepository userRepository;
+  private final UserFinder userFinder;
   private final FavoriteFinder favoriteFinder;
   private final FavoriteStoreDeleter favoriteStoreDeleter;
+  private final FavoriteDeleter favoriteDeleter;
 
   @Transactional
-  public Long create(final FavoritePostCommand favoritePostCommand) {
+  public void create(final FavoritePostCommand favoritePostCommand) {
 
-    long userId = favoritePostCommand.userId();
-    User findUser = userRepository.findById(userId).get();
+    User findUser = userFinder.getUser(favoritePostCommand.userId());
 
     String title = favoritePostCommand.title();
     String details = String.join(" ", favoritePostCommand.details());
 
-    Favorite favorite = Favorite.create(findUser, title, details); // null -> 유저 들어갈 예정
-    favoriteRepository.save(favorite);
-
-    return favorite.getId();
+    favoriteRepository.save(Favorite.create(findUser, title, details));
 
   }
 
   @Transactional
-  public void delete(final FavoriteDeleteRequest favoriteDeleteRequest) {
+  public void deleteFavorites(final FavoriteDeleteRequest favoriteDeleteRequest) {
 
-    List<Long> favoriteIds = favoriteDeleteRequest.favoriteIds();
-
-    List<Favorite> favorites = favoriteIds.stream()
+    List<Favorite> favorites = favoriteDeleteRequest.favoriteIds().stream()
         .map(favoriteFinder::findById)
         .toList();
 
-    // 관련된 favoriteStores batch 설정해서 알아서 가져올 듯
-    List<Long> favoriteStores =  favorites.stream()
-        .flatMap(favorite -> favorite.getFavoriteStores().stream())
-        .map(FavoriteStore::getId).toList();
+    favoriteStoreDeleter.deleteAllByFavorites(favorites);
+    favoriteDeleter.deleteAll(favorites);
 
-    if (!favorites.isEmpty()) {
-      favoriteRepository.deleteAll(favorites);
-      favoriteStoreDeleter.deleteByIds(favoriteStores);
-    } else {
-    throw new OperationFailedException(ErrorCode.INTERNAL_SERVER_ERROR);
-  }
   }
 }

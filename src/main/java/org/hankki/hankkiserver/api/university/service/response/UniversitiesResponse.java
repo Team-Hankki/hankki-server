d8771ab0bbbcade9a0f1529d47547b704954b864
Parkjyun <jaeyun0507@naver.com>
2024-07-10 23:23:59 +0900

    [refac] delete unused import statement

--- src/main/java/org/hankki/hankkiserver/api/university/service/response/UniversitiesResponse.java
+++ src/main/java/org/hankki/hankkiserver/api/university/service/response/UniversitiesResponse.java
@@ -1,7 +1,5 @@
 package org.hankki.hankkiserver.api.university.service.response;
 
-import org.hankki.hankkiserver.domain.university.model.University;
-
 import java.util.List;
 
 public record UniversitiesResponse(List<UniversityFindResponse> universities) {

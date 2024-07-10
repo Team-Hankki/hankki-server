247f8537d428d0c5db702eeab86f18b5d1e2ddf7
kgy1008 <gayeon1008@g.skku.edu>
2024-07-10 14:03:07 +0900

    [feat] change ResponseEntity to HankkiResponse

--- src/main/java/org/hankki/hankkiserver/api/report/controller/ReportController.java
+++ src/main/java/org/hankki/hankkiserver/api/report/controller/ReportController.java
@@ -1,8 +1,10 @@
 package org.hankki.hankkiserver.api.report.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.hankki.hankkiserver.api.dto.HankkiResponse;
 import org.hankki.hankkiserver.api.report.service.ReportService;
 import org.hankki.hankkiserver.api.report.service.response.ReportServiceResponse;
+import org.hankki.hankkiserver.common.code.CommonSuccessCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,9 @@ public class ReportController {
     private final ReportService reportService;
 
     @GetMapping("/reports/count")
-    public ResponseEntity<ReportServiceResponse> getReportCounts() {
+    public HankkiResponse<ReportServiceResponse> getReportCounts() {
         long reportCount = reportService.getMyReportSequence();
         ReportServiceResponse response = new ReportServiceResponse(reportCount);
-        return ResponseEntity.ok(response);
+        return HankkiResponse.success(CommonSuccessCode.OK, response);
     }
 }

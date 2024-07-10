5872b5823b55984058d2ba5f6fdc3e943b8a39ec
Parkjyun <jaeyun0507@naver.com>
2024-07-11 02:06:32 +0900

    [feat] create enums for get filter information api

--- src/main/java/org/hankki/hankkiserver/domain/store/model/StoreCategory.java
+++ src/main/java/org/hankki/hankkiserver/domain/store/model/StoreCategory.java
@@ -2,21 +2,24 @@ package org.hankki.hankkiserver.domain.store.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public enum StoreCategory {
 
-    KOREAN("한식"),
-    CHINESE("중식"),
-    JAPANESE("일식"),
-    WESTERN("양식"),
-    CONVENIENCEFOOD("간편식"),
-    BUNSIK("분식"),
-    SALAD("샐러드"),
-    FASTFOOD("패스트푸드"),
-    ETC("기타");
+    KOREAN("한식", "www."),
+    CHINESE("중식", "www."),
+    JAPANESE("일식", "www."),
+    WESTERN("양식", "www."),
+    CONVENIENCEFOOD("간편식", "www."),
+    BUNSIK("분식", "www."),
+    SALADSANDWICH("샐러드", "www."),
+    FASTFOOD("패스트푸드", "www."),
+    WORLD("세계음식", "www.");
 
     private final String name;
+    private final String url;
 
 }

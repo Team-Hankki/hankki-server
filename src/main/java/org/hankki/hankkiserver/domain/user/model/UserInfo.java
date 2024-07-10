8ed7d37f09449c9f8dda4f3151987d56671e9702
kgy1008 <gayeon1008@g.skku.edu>
2024-07-11 01:13:42 +0900

    [refac] delete cascade

--- src/main/java/org/hankki/hankkiserver/domain/user/model/UserInfo.java
+++ src/main/java/org/hankki/hankkiserver/domain/user/model/UserInfo.java
@@ -23,7 +23,7 @@ public class UserInfo {
 
     private String refreshToken;
 
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 

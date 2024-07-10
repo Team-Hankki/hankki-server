2bb59a6e3a1c1c8c354ff2323d6bf49648b2afb5
Parkjyun <jaeyun0507@naver.com>
2024-07-11 02:08:53 +0900

    [feat] create get filter information apis

---
+++ src/main/java/org/hankki/hankkiserver/api/store/controller/StoreController.java
@@ -0,0 +1,35 @@
+package org.hankki.hankkiserver.api.store.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.hankki.hankkiserver.api.dto.HankkiResponse;
+import org.hankki.hankkiserver.api.store.service.StoreQueryService;
+import org.hankki.hankkiserver.api.store.service.response.CategoriesResponse;
+import org.hankki.hankkiserver.api.store.service.response.PriceCategoriesResponse;
+import org.hankki.hankkiserver.api.store.service.response.SortOptionsResponse;
+import org.hankki.hankkiserver.common.code.CommonSuccessCode;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class StoreController {
+
+    private final StoreQueryService storeQueryService;
+
+    @GetMapping("/stores/categories")
+    public HankkiResponse<CategoriesResponse> getCategories() {
+        return HankkiResponse.success(CommonSuccessCode.OK, storeQueryService.getCategories());
+    }
+
+    @GetMapping("/stores/sort-options")
+    public HankkiResponse<SortOptionsResponse> getSortOptions() {
+        return HankkiResponse.success(CommonSuccessCode.OK, storeQueryService.getSortOptions());
+    }
+
+    @GetMapping("/stores/price-categories")
+    public HankkiResponse<PriceCategoriesResponse> getPrices() {
+        return HankkiResponse.success(CommonSuccessCode.OK, storeQueryService.getPriceCategories());
+    }
+}

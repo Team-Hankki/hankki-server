f145e4c4573d2bca2b05398329cb54e39eeaa03d
Parkjyun <jaeyun0507@naver.com>
2024-07-11 23:24:03 +0900

    [reface] create line break in StoreThumbnailResponse.java

--- src/main/java/org/hankki/hankkiserver/api/store/service/response/StoreThumbnailResponse.java
+++ src/main/java/org/hankki/hankkiserver/api/store/service/response/StoreThumbnailResponse.java
@@ -2,7 +2,13 @@ package org.hankki.hankkiserver.api.store.service.response;
 
 import org.hankki.hankkiserver.domain.store.model.Store;
 
-public record StoreThumbnailResponse(long id, String name, String category, int lowestPrice, int heartCount) {
+public record StoreThumbnailResponse(
+        long id,
+        String name,
+        String category,
+        int lowestPrice,
+        int heartCount
+) {
     public static StoreThumbnailResponse of(Store store) {
         return new StoreThumbnailResponse(
                 store.getId(),

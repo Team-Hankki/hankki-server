7ae9c37f79842dbc8444a6058f2485711016748b
Parkjyun <jaeyun0507@naver.com>
2024-07-12 09:49:55 +0900

    [feat] create menu find logic

--- src/main/java/org/hankki/hankkiserver/domain/menu/repository/MenuRepository.java
+++ src/main/java/org/hankki/hankkiserver/domain/menu/repository/MenuRepository.java
@@ -1,7 +1,11 @@
 package org.hankki.hankkiserver.domain.menu.repository;
 
 import org.hankki.hankkiserver.domain.menu.model.Menu;
+import org.hankki.hankkiserver.domain.store.model.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    List<Menu> findAllByStore(Store store);
 }

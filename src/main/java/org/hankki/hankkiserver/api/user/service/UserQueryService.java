93957683e9736621e61f0f91960b34a471a89386
kgy1008 <gayeon1008@g.skku.edu>
2024-07-14 15:29:36 +0900

    [feat] logic for get user nickname and profile

--- src/main/java/org/hankki/hankkiserver/api/user/service/UserQueryService.java
+++ src/main/java/org/hankki/hankkiserver/api/user/service/UserQueryService.java
@@ -1,9 +1,13 @@
 package org.hankki.hankkiserver.api.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.hankki.hankkiserver.api.auth.service.UserFinder;
+import org.hankki.hankkiserver.api.auth.service.UserInfoFinder;
+import org.hankki.hankkiserver.api.user.service.response.UserProfileAndNicknmaeResponse;
 import org.hankki.hankkiserver.api.user.service.response.UserUniversityFindResponse;
 import org.hankki.hankkiserver.common.code.UserUniversityErrorCode;
 import org.hankki.hankkiserver.common.exception.NotFoundException;
+import org.hankki.hankkiserver.domain.user.model.UserInfo;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,10 +16,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserQueryService {
 
     private final UserUniversityFinder userUniversityFinder;
+    private final UserInfoFinder userInfoFinder;
 
     @Transactional(readOnly = true)
     public UserUniversityFindResponse findUserUniversity(Long userId) {
         return UserUniversityFindResponse.of(userUniversityFinder.findByUserId(userId)
                 .orElseThrow(() -> new NotFoundException(UserUniversityErrorCode.USER_UNIVERSITY_NOT_FOUND)));
+    }
+
+    public UserProfileAndNicknmaeResponse getUserProfileAndNickname(final Long userId) {
+        UserInfo userInfo = userInfoFinder.getUserInfo(userId);
+        return UserProfileAndNicknmaeResponse.of(userInfo.getNickname(), userInfo.getProfileImageUrl());
     }
 }

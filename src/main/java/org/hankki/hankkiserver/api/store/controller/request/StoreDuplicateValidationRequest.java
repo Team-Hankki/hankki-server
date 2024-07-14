bf8d8b8ea906000c0c34829b97d6400534839ec0
kgy1008 <gayeon1008@g.skku.edu>
2024-07-15 03:08:37 +0900

    [feat] add universityId in request

--- src/main/java/org/hankki/hankkiserver/api/store/controller/request/StoreDuplicateValidationRequest.java
+++ src/main/java/org/hankki/hankkiserver/api/store/controller/request/StoreDuplicateValidationRequest.java
@@ -1,6 +1,7 @@
 package org.hankki.hankkiserver.api.store.controller.request;
 
 public record StoreDuplicateValidationRequest(
+        Long id,
         Double latitude,
         Double longitude
 ) {

aac180ede6c91067c23d1a56c091aaa33c7cf31c
PicturePark1101 <seojin5565@gmail.com>
2024-07-14 23:45:50 +0900

    [feat] imple repository

--- src/main/java/org/hankki/hankkiserver/domain/report/repository/ReportRepository.java
+++ src/main/java/org/hankki/hankkiserver/domain/report/repository/ReportRepository.java
@@ -1,7 +1,13 @@
 package org.hankki.hankkiserver.domain.report.repository;
 
+import java.util.List;
 import org.hankki.hankkiserver.domain.report.model.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
+
+  @Query("select r from Report r join fetch r.store where r.user.id = :userId and r.store.isDeleted = false")
+  List<Report> findAllByUserIdWithStore(@Param("userId") Long userId);
 }

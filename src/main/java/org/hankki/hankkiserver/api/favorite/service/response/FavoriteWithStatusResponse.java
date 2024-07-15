c0769a1ed2fe25fcb61f22c27cda96577ee3cfe5
PicturePark1101 <seojin5565@gmail.com>
2024-07-16 05:30:47 +0900

    [refac] change response type and query

--- src/main/java/org/hankki/hankkiserver/api/favorite/service/response/FavoriteWithStatusResponse.java
+++ src/main/java/org/hankki/hankkiserver/api/favorite/service/response/FavoriteWithStatusResponse.java
@@ -3,19 +3,20 @@ package org.hankki.hankkiserver.api.favorite.service.response;
 import java.util.List;
 import org.hankki.hankkiserver.api.favorite.service.response.util.FavoriteResponseUtil;
 import org.hankki.hankkiserver.domain.favorite.model.Favorite;
+import org.hankki.hankkiserver.domain.favorite.model.FavoriteImage;
 
 public record FavoriteWithStatusResponse(
     Long id,
     String title,
-    String imageUrl,
+    FavoriteImage imageType,
     List<String> details,
-    boolean isReported
+    boolean isAdded
 ) {
   public static FavoriteWithStatusResponse of(Favorite favorite, boolean isReported) {
     return new FavoriteWithStatusResponse(
         favorite.getId(),
         favorite.getName(),
-        favorite.getImageUrl(),
+        favorite.getImageType(),
         FavoriteResponseUtil.getDetail(favorite.getDetail()),
         isReported);
   }

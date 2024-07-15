9c6bbc97048242362b144e70103ae1b8182e8e2a
kgy1008 <gayeon1008@g.skku.edu>
2024-07-15 22:57:40 +0900

    [refac] delete unnecessary blank and imports

--- src/main/java/org/hankki/hankkiserver/api/store/controller/StoreController.java
+++ src/main/java/org/hankki/hankkiserver/api/store/controller/StoreController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
--- src/main/java/org/hankki/hankkiserver/domain/heart/model/Heart.java
+++ src/main/java/org/hankki/hankkiserver/domain/heart/model/Heart.java
@@ -38,5 +38,4 @@ public class Heart {
                 .store(store)
                 .build();
     }
-
 }

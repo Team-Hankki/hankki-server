ca38644703759eb341d48f80df32829c5f9ff2a8
PicturePark1101 <seojin5565@gmail.com>
2024-07-15 16:39:01 +0900

    [refac] change FavoriteImage variable name

--- src/main/java/org/hankki/hankkiserver/domain/favorite/model/FavoriteImage.java
+++ src/main/java/org/hankki/hankkiserver/domain/favorite/model/FavoriteImage.java
@@ -2,25 +2,24 @@ package org.hankki.hankkiserver.domain.favorite.model;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.parameters.P;
 
 @Getter
 @RequiredArgsConstructor
 public enum FavoriteImage {
 
   BASE_IMAGE,
-  ONE_TO_FIVE_IMAGE,
-  SIX_TO_TEN_IMAGE,
-  ELEVEN_AND_ABOVE_IMAGE;
+  ONE_TO_FIVE,
+  SIX_TO_TEN,
+  ELEVEN_AND_ABOVE;
 
   public static FavoriteImage getType(final int countFavoriteStore) {
     if (countFavoriteStore == 0) {
       return BASE_IMAGE;
     } else if (countFavoriteStore >= 1 && countFavoriteStore <= 5) {
-      return ONE_TO_FIVE_IMAGE;
+      return ONE_TO_FIVE;
     } else if (countFavoriteStore >= 6 && countFavoriteStore <= 10) {
-      return SIX_TO_TEN_IMAGE;
+      return SIX_TO_TEN;
     }
-    return ELEVEN_AND_ABOVE_IMAGE;
+    return ELEVEN_AND_ABOVE;
   }
 }

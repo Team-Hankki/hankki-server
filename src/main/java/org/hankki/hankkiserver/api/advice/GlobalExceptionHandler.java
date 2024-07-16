cc3c2803684c423da7e37f70670092daffdbe439
Parkjyun <jaeyun0507@naver.com>
2024-07-16 22:11:11 +0900

    [feat] add exceptions in GlobalExceptionHandler.java

--- src/main/java/org/hankki/hankkiserver/api/advice/GlobalExceptionHandler.java
+++ src/main/java/org/hankki/hankkiserver/api/advice/GlobalExceptionHandler.java
@@ -3,13 +3,20 @@ package org.hankki.hankkiserver.api.advice;
 import lombok.extern.slf4j.Slf4j;
 import org.hankki.hankkiserver.api.dto.HankkiResponse;
 import org.hankki.hankkiserver.common.code.BusinessErrorCode;
+import org.hankki.hankkiserver.common.code.StoreErrorCode;
 import org.hankki.hankkiserver.common.exception.BadRequestException;
 import org.hankki.hankkiserver.common.exception.ConflictException;
 import org.hankki.hankkiserver.common.exception.NotFoundException;
 import org.hankki.hankkiserver.common.exception.UnauthorizedException;
+<<<<<<< HEAD
+=======
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+>>>>>>> 9dbf784 ([feat] add exceptions in GlobalExceptionHandler.java)
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @RestControllerAdvice
 @Slf4j
@@ -45,9 +52,21 @@ public class GlobalExceptionHandler {
         return HankkiResponse.fail(BusinessErrorCode.BAD_REQUEST);
     }
 
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public HankkiResponse<Void> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
+        log.error("handleMaxUploadSizeExceededException() in GlobalExceptionHandler throw MaxUploadSizeExceededException : {}", e.getMessage());
+        return HankkiResponse.fail(StoreErrorCode.STORE_FILE_SIZE_EXCEEDED);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public HankkiResponse<Void> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("handleMethodArgumentNotValidException() in GlobalExceptionHandler throw MethodArgumentNotValidException : {}", e.getMessage());
+        return HankkiResponse.fail(BusinessErrorCode.BAD_REQUEST);
+    }
+
     @ExceptionHandler(Exception.class)
     public HankkiResponse<Void> handleException(Exception e) {
-        log.error("handleException() in GlobalExceptionHandler throw [{}] : {}, {}", e.getClass(), e.getMessage(), e.getStackTrace());
+        log.error("handleException() in GlobalExceptionHandler throw Exception [{}] : {}", e.getClass() , e.getMessage());
         return HankkiResponse.fail(BusinessErrorCode.INTERNAL_SERVER_ERROR);
 
     }

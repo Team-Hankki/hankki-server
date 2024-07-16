9279bedcf1cab46272c25b27c3f159d33167cd39
Parkjyun <98092394+Parkjyun@users.noreply.github.com>
2024-07-16 23:22:26 +0900

    [fix] edit PriceCategory.java enum's name (#94)

--- src/main/java/org/hankki/hankkiserver/api/store/parameter/PriceCategory.java
+++ src/main/java/org/hankki/hankkiserver/api/store/parameter/PriceCategory.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum PriceCategory {
 
     K6("6000원 이하", "6000"),
-    k8("6000~8000", "8000");
+    k8("6000~8000원", "8000");
 
     private final String name;
     private final String maxPrice;

ae9423d66ead1ef58eb7f7f709b0b5fad457dff9
Parkjyun <jaeyun0507@naver.com>
2024-07-16 22:04:25 +0900

    [feat] create ReportUpdater

---
+++ src/main/java/org/hankki/hankkiserver/api/report/service/ReportUpdater.java
@@ -0,0 +1,17 @@
+package org.hankki.hankkiserver.api.report.service;
+
+import lombok.RequiredArgsConstructor;
+import org.hankki.hankkiserver.domain.report.model.Report;
+import org.hankki.hankkiserver.domain.report.repository.ReportRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ReportUpdater {
+
+    private final ReportRepository repository;
+
+    public void save(Report report) {
+        repository.save(report);
+    }
+}

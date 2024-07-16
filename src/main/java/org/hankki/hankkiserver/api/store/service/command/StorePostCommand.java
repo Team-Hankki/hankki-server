011b5e4d000e7e640dae5c8247533ac789a2cb84
Parkjyun <jaeyun0507@naver.com>
2024-07-16 22:07:20 +0900

    [feat] create command for Store post api

---
+++ src/main/java/org/hankki/hankkiserver/api/store/service/command/StorePostCommand.java
@@ -0,0 +1,32 @@
+package org.hankki.hankkiserver.api.store.service.command;
+
+import org.hankki.hankkiserver.api.store.controller.request.MenuPostRequest;
+import org.hankki.hankkiserver.api.store.controller.request.StorePostRequest;
+import org.hankki.hankkiserver.domain.store.model.StoreCategory;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public record StorePostCommand (
+        MultipartFile image,
+        String name,
+        String address,
+        StoreCategory category,
+        double latitude,
+        double longitude,
+        Long universityId,
+        Long userId,
+        List<MenuPostRequest> menus
+) {
+    public static StorePostCommand of(final MultipartFile image, final StorePostRequest request, final Long userId) {
+        return new StorePostCommand(image,
+                request.name(),
+                request.address(),
+                request.category(),
+                request.latitude(),
+                request.longitude(),
+                request.universityId(),
+                userId,
+                request.menus());
+    }
+}

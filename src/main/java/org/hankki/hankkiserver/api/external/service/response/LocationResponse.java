816a9c586b5bd1476cf510f053789635f46307cd
Parkjyun <98092394+Parkjyun@users.noreply.github.com>
2024-07-17 00:26:50 +0900

    [fix] fix location search api to return location names without tag (#97)

--- src/main/java/org/hankki/hankkiserver/api/external/service/response/LocationResponse.java
+++ src/main/java/org/hankki/hankkiserver/api/external/service/response/LocationResponse.java
@@ -5,6 +5,10 @@ import org.hankki.hankkiserver.external.openfeign.naver.dto.NaverLocationInfo;
 public record LocationResponse(double latitude, double longitude, String name, String address) {
     private static final int RATIO = 10000000;
     public static LocationResponse of(NaverLocationInfo location) {
-        return new LocationResponse(location.mapy()/RATIO, location.mapx()/RATIO, location.title(), location.address());
+        return new LocationResponse(location.mapy()/RATIO, location.mapx()/RATIO, trim(location.title()), location.address());
+    }
+
+    private static String trim(String string) {
+        return string.replaceAll("<[^>]*>", "");
     }
 }

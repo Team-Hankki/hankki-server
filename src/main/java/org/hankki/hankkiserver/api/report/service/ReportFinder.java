adaf0bd04702b7efa6d79fc6cfc11b499bbf4acb
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2024-07-17 18:01:49 +0900

    [feat] 마이페이지 - 사용자가 제보한 가게 API (#84)
    * [feat] setting response dto
    
    * [feat] imple repository
    
    * [feat] imple Service logic
    
    * [feat] imple Controller logic
    
    * [feat] remove duplicate stores
    
    * [refac] add fetch join to StoreImage
    
    * [refac] change method name
    
    * [feat] add @Transactional(readOnly = true)

--- src/main/java/org/hankki/hankkiserver/api/report/service/ReportFinder.java
+++ src/main/java/org/hankki/hankkiserver/api/report/service/ReportFinder.java
@@ -1,9 +1,10 @@
 package org.hankki.hankkiserver.api.report.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.hankki.hankkiserver.domain.report.model.Report;
 import org.hankki.hankkiserver.domain.report.repository.ReportRepository;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -13,5 +14,9 @@ public class ReportFinder {
 
     public long getReportCount() {
         return reportRepository.count();
+    }
+
+    public List<Report> findAllByUserId(final Long userId) {
+        return reportRepository.findAllByUserIdWithStoreAndStoreImage(userId);
     }
 }
--- src/main/java/org/hankki/hankkiserver/domain/report/repository/ReportRepository.java
+++ src/main/java/org/hankki/hankkiserver/domain/report/repository/ReportRepository.java
@@ -1,7 +1,13 @@
 package org.hankki.hankkiserver.domain.report.repository;
 
+import java.util.List;
 import org.hankki.hankkiserver.domain.report.model.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
+
+  @Query("select r from Report r join fetch r.store join fetch r.store.images where r.user.id = :userId and r.store.isDeleted = false order by r.createdAt desc")
+  List<Report> findAllByUserIdWithStoreAndStoreImage(@Param("userId") Long userId);
 }

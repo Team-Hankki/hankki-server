0c5bfaccc4547be6a21234287175d85c0da8e29a
kgy1008 <gayeon1008@g.skku.edu>
2024-07-19 02:46:07 +0900

    [refac] change sequence of response data

--- src/main/java/org/hankki/hankkiserver/external/openfeign/apple/dto/AppleTokenResponse.java
+++ src/main/java/org/hankki/hankkiserver/external/openfeign/apple/dto/AppleTokenResponse.java
@@ -1,25 +1,15 @@
 package org.hankki.hankkiserver.external.openfeign.apple.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-@JsonIgnoreProperties(ignoreUnknown = true)
 public record AppleTokenResponse(
         String accessToken,
+        String tokenType,
         String expiresIn,
-        String idToken,
         String refreshToken,
-        String tokenType) {
-
-    public static AppleTokenResponse of(
-            final String accessToken,
-            final String expiresIn,
-            final String idToken,
-            final String refreshToken,
-            final String tokenType) {
-        return new AppleTokenResponse(accessToken, expiresIn, idToken, refreshToken, tokenType);
-    }
+        String idToken
+        ) {
 }
 

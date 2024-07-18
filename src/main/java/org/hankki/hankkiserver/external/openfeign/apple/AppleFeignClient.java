87865e4179d02aee855b88434198317ac44f11cd
kgy1008 <gayeon1008@g.skku.edu>
2024-07-19 04:28:30 +0900

    [fix] 회원 탈퇴 에러 수정 (#137)
    * [refac] delete unused request dto
    
    * [fix] fix changing profile after revoke and rejoin
    
    * [fix] fix feign request logic for apple login

--- src/main/java/org/hankki/hankkiserver/external/openfeign/apple/AppleFeignClient.java
+++ src/main/java/org/hankki/hankkiserver/external/openfeign/apple/AppleFeignClient.java
@@ -1,13 +1,12 @@
 package org.hankki.hankkiserver.external.openfeign.apple;
 
 import org.hankki.hankkiserver.external.openfeign.apple.dto.ApplePublicKeys;
-import org.hankki.hankkiserver.external.openfeign.apple.dto.AppleRevokeRequest;
-import org.hankki.hankkiserver.external.openfeign.apple.dto.AppleTokenRequest;
 import org.hankki.hankkiserver.external.openfeign.apple.dto.AppleTokenResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 
 @FeignClient(name = "appleClient", url = "https://appleid.apple.com/auth")
 public interface AppleFeignClient {
@@ -16,8 +15,14 @@ public interface AppleFeignClient {
     ApplePublicKeys getApplePublicKey();
 
     @PostMapping(value = "/token", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    AppleTokenResponse getAppleTokens(AppleTokenRequest request);
+    AppleTokenResponse getAppleTokens(@RequestPart(value = "code") String code,
+                                      @RequestPart(value = "client_id") String client_id,
+                                      @RequestPart(value = "client_secret") String client_secret,
+                                      @RequestPart(value = "grant_type") String grant_type);
 
     @PostMapping(value = "/revoke", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    void revoke(AppleRevokeRequest request);
+    void revoke(@RequestPart(value = "token") String token,
+                @RequestPart(value = "client_id") String client_id,
+                @RequestPart(value = "client_secret") String client_secret,
+                @RequestPart(value = "token_type_hint") String token_type_hint);
 }

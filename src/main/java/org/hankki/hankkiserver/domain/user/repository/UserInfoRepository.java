75ac09c80774b9de1ea9cae856a38b13adb68ce5
kgy1008 <gayeon1008@g.skku.edu>
2024-08-09 01:56:53 +0900

    [refac] delete unused login

--- src/main/java/org/hankki/hankkiserver/domain/user/repository/UserInfoRepository.java
+++ src/main/java/org/hankki/hankkiserver/domain/user/repository/UserInfoRepository.java
@@ -3,16 +3,10 @@ package org.hankki.hankkiserver.domain.user.repository;
 import org.hankki.hankkiserver.domain.user.model.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
 import java.util.Optional;
 
 public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
 
     Optional<UserInfo> findByUserId(Long userId);
 
-    @Modifying
-    @Query("UPDATE UserInfo ui SET ui.refreshToken = null, ui.nickname = '알 수 없음' WHERE ui.user.id = :userId")
-    void softDeleteByUserId(Long userId);
 }

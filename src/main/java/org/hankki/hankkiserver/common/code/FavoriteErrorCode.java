9bf9ccd1797e3b70862eeefe72aa9b6df7f80dad
PicturePark1101 <seojin5565@gmail.com>
2024-08-11 23:12:29 +0900

    [feat] add 409 error code

--- src/main/java/org/hankki/hankkiserver/common/code/FavoriteErrorCode.java
+++ src/main/java/org/hankki/hankkiserver/common/code/FavoriteErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum FavoriteErrorCode implements ErrorCode {
 
-  FAVORITE_NOT_FOUND(HttpStatus.NOT_FOUND, "등록되지 않은 족보입니다.");
+  FAVORITE_NOT_FOUND(HttpStatus.NOT_FOUND, "등록되지 않은 족보입니다."),
+  FAVORITE_TITLE_IS_EXIST(HttpStatus.CONFLICT, "이미 존재하는 족보 제목입니다.");
 
   private final HttpStatus httpStatus;
   private final String message;

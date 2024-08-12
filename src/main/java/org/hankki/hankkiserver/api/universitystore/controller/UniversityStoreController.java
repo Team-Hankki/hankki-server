d781289fe5c7b03da5d5a9e8c0911a1cab1ec37c
kgy1008 <gayeon1008@g.skku.edu>
2024-08-13 00:31:14 +0900

    [feat] logic for add store in report table

--- src/main/java/org/hankki/hankkiserver/api/universitystore/controller/UniversityStoreController.java
+++ src/main/java/org/hankki/hankkiserver/api/universitystore/controller/UniversityStoreController.java
@@ -3,8 +3,9 @@ package org.hankki.hankkiserver.api.universitystore.controller;
 import lombok.RequiredArgsConstructor;
 import org.hankki.hankkiserver.api.dto.HankkiResponse;
 import org.hankki.hankkiserver.api.universitystore.controller.request.UniviersityStoreCreateRequest;
-import org.hankki.hankkiserver.api.universitystore.service.UniversitystoreCommandService;
+import org.hankki.hankkiserver.api.universitystore.service.UniversityStoreCommandService;
 import org.hankki.hankkiserver.api.universitystore.service.command.UniversityStorePostCommand;
+import org.hankki.hankkiserver.auth.UserId;
 import org.hankki.hankkiserver.common.code.CommonSuccessCode;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,11 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class UniversityStoreController {
 
-    private final UniversitystoreCommandService universitystoreCommandService;
+    private final UniversityStoreCommandService universitystoreCommandService;
 
-    @PostMapping("/universitystores")
-    public HankkiResponse<Void> createStore(@RequestBody final UniviersityStoreCreateRequest request) {
-        universitystoreCommandService.createUniversityStore(UniversityStorePostCommand.of(request.storeId(), request.universityId()));
+    @PostMapping("/university-stores")
+    public HankkiResponse<Void> createStore(@UserId final Long userId,
+                                            @RequestBody final UniviersityStoreCreateRequest request) {
+        universitystoreCommandService.createUniversityStore(UniversityStorePostCommand.of(userId, request.storeId(), request.universityId()));
         return HankkiResponse.success(CommonSuccessCode.CREATED);
     }
 }

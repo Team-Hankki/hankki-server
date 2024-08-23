7ac449e6c62a3793f4f326c356b2781a9d7a7367
Parkjyun <98092394+Parkjyun@users.noreply.github.com>
2024-08-23 10:29:47 +0900

    [fix] change dtos to accept null for universityId (#172)

--- src/main/java/org/hankki/hankkiserver/api/user/controller/request/UserUniversityPostRequest.java
+++ src/main/java/org/hankki/hankkiserver/api/user/controller/request/UserUniversityPostRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserUniversityPostRequest(
-        long universityId,
+        Long universityId,
         @NotBlank @Size(min = 3, max = 20)
         String name,
         double longitude,

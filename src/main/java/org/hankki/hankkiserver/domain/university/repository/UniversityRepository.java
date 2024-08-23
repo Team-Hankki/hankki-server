6351f8fb1c466068ce22eaf41c462b5142be64a2
Parkjyun <98092394+Parkjyun@users.noreply.github.com>
2024-08-24 04:09:49 +0900

    [fix] fix the logic to sort universities upon retrieval (#174)

--- src/main/java/org/hankki/hankkiserver/domain/university/repository/UniversityRepository.java
+++ src/main/java/org/hankki/hankkiserver/domain/university/repository/UniversityRepository.java
@@ -3,5 +3,8 @@ package org.hankki.hankkiserver.domain.university.repository;
 import org.hankki.hankkiserver.domain.university.model.University;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UniversityRepository extends JpaRepository<University, Long> {
+    List<University> findAllByOrderByName();
 }

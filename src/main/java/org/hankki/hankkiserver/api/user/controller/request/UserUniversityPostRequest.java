2307598a3b58be46eb54f2976978dab1c9930afe
Parkjyun <98092394+Parkjyun@users.noreply.github.com>
2024-08-24 06:33:10 +0900

    [fix] fix validation for minimum size of name in UserUniversityPostRequest (#176)

--- src/main/java/org/hankki/hankkiserver/api/user/controller/request/UserUniversityPostRequest.java
+++ src/main/java/org/hankki/hankkiserver/api/user/controller/request/UserUniversityPostRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Size;
 
 public record UserUniversityPostRequest(
         Long universityId,
-        @NotBlank @Size(min = 3, max = 20)
+        @NotBlank @Size(min = 2, max = 20)
         String name,
         double longitude,
         double latitude

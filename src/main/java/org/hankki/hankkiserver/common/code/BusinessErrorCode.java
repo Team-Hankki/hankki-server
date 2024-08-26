7688a16fa137ce1ab88741eabe1d39cc2cb47b1b
Parkjyun <jaeyun0507@naver.com>
2024-08-26 12:24:25 +0900

    [feat] add errorcode

--- src/main/java/org/hankki/hankkiserver/common/code/BusinessErrorCode.java
+++ src/main/java/org/hankki/hankkiserver/common/code/BusinessErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum BusinessErrorCode implements ErrorCode {
 
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 자원을 찾을 수 없습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
 
     private final HttpStatus httpStatus;

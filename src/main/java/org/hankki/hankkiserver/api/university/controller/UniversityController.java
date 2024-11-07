9c96bb12c897769b04a85b5168649d4a8a993e58
PicturePark1101 <seojin5565@gmail.com>
2024-11-08 01:20:54 +0900

    [feat] implements controller

--- src/main/java/org/hankki/hankkiserver/api/university/controller/UniversityController.java
+++ src/main/java/org/hankki/hankkiserver/api/university/controller/UniversityController.java
@@ -2,10 +2,15 @@ package org.hankki.hankkiserver.api.university.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.hankki.hankkiserver.api.dto.HankkiResponse;
+import org.hankki.hankkiserver.api.university.request.UniversitiesPostRequest;
+import org.hankki.hankkiserver.api.university.service.UniversityCommandService;
 import org.hankki.hankkiserver.api.university.service.UniversityQueryService;
+import org.hankki.hankkiserver.api.university.service.command.UniversitiesPostCommand;
 import org.hankki.hankkiserver.api.university.service.response.UniversitiesResponse;
 import org.hankki.hankkiserver.common.code.CommonSuccessCode;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class UniversityController {
 
     private final UniversityQueryService universityQueryService;
+    private final UniversityCommandService universityCommandService;
 
     @GetMapping("/universities")
     public HankkiResponse<UniversitiesResponse> getUniversities() {
         return HankkiResponse.success(CommonSuccessCode.OK, universityQueryService.findAllByOrderByName());
+    }
+
+    @PostMapping("/universities")
+    public HankkiResponse<Void> createUniversities(@RequestBody UniversitiesPostRequest universitiesPostRequest) {
+        universityCommandService.createUniversities(UniversitiesPostCommand.of(universitiesPostRequest));
+        return HankkiResponse.success(CommonSuccessCode.CREATED);
     }
 }

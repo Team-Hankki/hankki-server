728a9a61fc73a825142983b56de7b0ea5cc681fd
kgy1008 <gayeon1008@g.skku.edu>
2024-12-02 05:15:22 +0900

    [feat] set retry configuration

---
+++ src/main/java/org/hankki/hankkiserver/api/config/RetryConfig.java
@@ -0,0 +1,9 @@
+package org.hankki.hankkiserver.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@EnableRetry
+@Configuration
+public class RetryConfig {
+}

eb245054af093a449d8c85260ccc0e39f4067952
PicturePark1101 <seojin5565@gmail.com>
2024-12-25 15:31:43 +0900

    [feat] Add response dto

--- src/main/java/org/hankki/hankkiserver/api/favorite/service/response/FavoriteSharedGetResponse.java
+++ src/main/java/org/hankki/hankkiserver/api/favorite/service/response/FavoriteSharedGetResponse.java
@@ -10,13 +10,32 @@ public record FavoriteSharedGetResponse(
     String title,
     List<String> details,
     String nickname,
-    List<FavoriteStoreResponse> stores
+    List<FavoriteSharedResponse> stores
 ) {
   public static FavoriteSharedGetResponse of(final Favorite favorite, final String nickname, final List<Store> stores) {
     return new FavoriteSharedGetResponse(
         favorite.getName(),
         transformDetail(favorite.getDetail()),
         nickname,
-        stores.stream().map(FavoriteStoreResponse::of).toList());
+        stores.stream().map(FavoriteSharedResponse::of).toList());
+  }
+
+  record FavoriteSharedResponse(
+      Long id,
+      String name,
+      String imageUrl,
+      String category,
+      int lowestPrice,
+      int heartCount
+  ) {
+    static FavoriteSharedResponse of(final Store store) {
+      return new FavoriteSharedResponse(
+          store.getId(),
+          store.getName(),
+          store.getImageUrlOrElseNull(),
+          store.getCategory().getName(),
+          store.getLowestPrice(),
+          store.getHeartCount());
+    }
   }
 }

032ddb5014a5c4ed71cdf987ec1f93159ee0a029
Parkjyun <jaeyun0507@naver.com>
2025-01-09 03:26:31 +0900

    [feat] create CustomUniversityStoreRepositoryImpl

---
+++ src/main/java/org/hankki/hankkiserver/domain/universitystore/repository/CustomUniversityStoreRepositoryImpl.java
@@ -0,0 +1,45 @@
+package org.hankki.hankkiserver.domain.universitystore.repository;
+
+import static org.hankki.hankkiserver.domain.store.model.QStore.store;
+import static org.hankki.hankkiserver.domain.universitystore.model.QUniversityStore.universityStore;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.hankki.hankkiserver.api.store.parameter.PriceCategory;
+import org.hankki.hankkiserver.api.store.parameter.SortOption;
+import org.hankki.hankkiserver.api.store.service.response.CustomCursor;
+import org.hankki.hankkiserver.domain.store.model.StoreCategory;
+import org.hankki.hankkiserver.domain.store.repository.CursorProvider;
+import org.hankki.hankkiserver.domain.store.repository.DynamicQueryProvider;
+import org.hankki.hankkiserver.domain.store.repository.OrderSpecifierProvider;
+import org.hankki.hankkiserver.domain.universitystore.model.UniversityStore;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUniversityStoreRepositoryImpl implements CustomUniversityStoreRepository {
+    private static final int PAGE_SIZE = 10;
+
+    private final JPAQueryFactory jpaQueryFactory;
+    private final OrderSpecifierProvider orderSpecifierProvider;
+    private final CursorProvider cursorProvider;
+    private final DynamicQueryProvider dynamicQueryProvider;
+    @Override
+    public List<UniversityStore> findAllWithStoreByCategoryAndLowestPriceAndUniversityIdAndIsDeletedFalseOrderBySortOptionsWithPaging(
+            StoreCategory category, PriceCategory priceCategory, Long universityId, SortOption sortOptions, CustomCursor cursor) {
+        return jpaQueryFactory
+                .select(universityStore)
+                .from(universityStore)
+                .join(universityStore.store, store).fetchJoin()
+                .where(cursorProvider.createCursorCondition(cursor, sortOptions))
+                .where(dynamicQueryProvider.eqUniversity(universityId),
+                        dynamicQueryProvider.eqCategory(category),
+                        dynamicQueryProvider.evaluatePriceCategory(priceCategory))
+                .where(universityStore.store.isDeleted.isFalse())
+                .orderBy(orderSpecifierProvider.createOrderSpecifierForPaging(sortOptions))
+                .limit(PAGE_SIZE)
+                .fetch();
+    }
+ }
+

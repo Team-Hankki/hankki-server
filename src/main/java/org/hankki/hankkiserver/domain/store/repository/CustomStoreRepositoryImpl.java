44aa15de660c171ceae7c038cdf683f05c9d0901
Parkjyun <jaeyun0507@naver.com>
2025-01-09 03:34:13 +0900

    [refactor] rename method used for dynamic query

--- src/main/java/org/hankki/hankkiserver/domain/store/repository/CustomStoreRepositoryImpl.java
+++ src/main/java/org/hankki/hankkiserver/domain/store/repository/CustomStoreRepositoryImpl.java
@@ -3,7 +3,6 @@ package org.hankki.hankkiserver.domain.store.repository;
 import static org.hankki.hankkiserver.domain.store.model.QStore.store;
 import static org.hankki.hankkiserver.domain.universitystore.model.QUniversityStore.universityStore;
 
-import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +21,7 @@ public class CustomStoreRepositoryImpl implements CustomStoreRepository {
     private final JPAQueryFactory jpaQueryFactory;
     private final OrderSpecifierProvider orderSpecifierProvider;
     private final CursorProvider cursorProvider;
+    private final DynamicQueryProvider dynamicQueryProvider;
 
     @Override
     public List<Store> findAllWithUniversityStoreByCategoryAndLowestPriceAndUniversityIdAndIsDeletedFalseOrderBySortOptions(
@@ -33,7 +33,9 @@ public class CustomStoreRepositoryImpl implements CustomStoreRepository {
                 .select(store)
                 .from(store)
                 .join(store.universityStores, universityStore).fetchJoin()
-                .where(eqCategory(category),eqUniversity(universityId), evaluatePriceCategory(priceCategory))
+                .where(dynamicQueryProvider.eqCategory(category),
+                        dynamicQueryProvider.hasUniversity(universityId),
+                        dynamicQueryProvider.evaluatePriceCategory(priceCategory))
                 .where(store.isDeleted.isFalse())
                 .orderBy(orderSpecifierProvider.createOrderSpecifier(sortOptions))
                 .fetch();
@@ -49,32 +51,11 @@ public class CustomStoreRepositoryImpl implements CustomStoreRepository {
                 .select(store)
                 .from(store)
                 .where(cursorProvider.createCursorCondition(cursor, sortOptions))
-                .where(eqCategory(category), evaluatePriceCategory(priceCategory)) // 필터
+                .where(dynamicQueryProvider.eqCategory(category),
+                        dynamicQueryProvider.evaluatePriceCategory(priceCategory)) // 필터
                 .where(store.isDeleted.isFalse())
                 .orderBy(orderSpecifierProvider.createOrderSpecifierForPaging(sortOptions))//정렬
                 .limit(PAGE_SIZE)
                 .fetch();
-    }
-
-    private BooleanExpression eqUniversity(Long university) {
-        if (university == null) {
-            return null;
-        }
-        return store.universityStores.any().university.id.eq(university);
-    }
-
-    private BooleanExpression eqCategory(StoreCategory category) {
-        if (category == null) {
-            return null;
-        }
-        return store.category.eq(category);
-    }
-
-    private BooleanExpression evaluatePriceCategory(PriceCategory priceCategory) {
-        if (priceCategory == null) {
-            return null;
-        }
-        return store.lowestPrice.goe(priceCategory.getMinPrice())
-                .and(store.lowestPrice.loe(priceCategory.getMaxPrice()));
     }
 }

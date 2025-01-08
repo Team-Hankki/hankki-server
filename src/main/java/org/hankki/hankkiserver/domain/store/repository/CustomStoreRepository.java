e1c1b559be8aa2bbb76b3fc8268f06177ae5b2a2
Parkjyun <jaeyun0507@naver.com>
2025-01-09 01:10:56 +0900

    [feat] add method signature related with paging

--- src/main/java/org/hankki/hankkiserver/domain/store/repository/CustomStoreRepository.java
+++ src/main/java/org/hankki/hankkiserver/domain/store/repository/CustomStoreRepository.java
@@ -2,11 +2,14 @@ package org.hankki.hankkiserver.domain.store.repository;
 
 import org.hankki.hankkiserver.api.store.parameter.PriceCategory;
 import org.hankki.hankkiserver.api.store.parameter.SortOption;
+import org.hankki.hankkiserver.api.store.service.response.CustomCursor;
 import org.hankki.hankkiserver.domain.store.model.Store;
 import org.hankki.hankkiserver.domain.store.model.StoreCategory;
 
 import java.util.List;
 
 public interface CustomStoreRepository {
-    List<Store> findStoreWithUniversityStoreByCategoryAndLowestPriceAndUniversityIdAndIsDeletedFalseOrderBySortOptionsWithPaging(StoreCategory category, PriceCategory priceCategory, Long universityId, SortOption sortOptions);
+    List<Store> findAllWithUniversityStoreByCategoryAndLowestPriceAndUniversityIdAndIsDeletedFalseOrderBySortOptions(StoreCategory category, PriceCategory priceCategory, Long universityId, SortOption sortOptions);
+    List<Store> findAllByCategoryAndLowestPriceAndUniversityIdAndIsDeletedFalseOrderBySortOptionsWithPaging(StoreCategory category, PriceCategory priceCategory, SortOption sortOptions, CustomCursor cursor);
+
 }

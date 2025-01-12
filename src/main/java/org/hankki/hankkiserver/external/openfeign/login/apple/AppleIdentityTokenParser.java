f8f5b6bc91fe2600dbf5cd15eb8fc9940c8a45bf
kgy1008 <gayeon1008@g.skku.edu>
2025-01-12 19:19:27 +0900

    [refac] use TypeReference for safe deserialization

--- src/main/java/org/hankki/hankkiserver/external/openfeign/login/apple/AppleIdentityTokenParser.java
+++ src/main/java/org/hankki/hankkiserver/external/openfeign/login/apple/AppleIdentityTokenParser.java
@@ -1,15 +1,19 @@
 package org.hankki.hankkiserver.external.openfeign.login.apple;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.jsonwebtoken.*;
-import org.hankki.hankkiserver.common.code.AuthErrorCode;
-import org.hankki.hankkiserver.common.exception.UnauthorizedException;
-import org.springframework.stereotype.Component;
-
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import java.security.PublicKey;
 import java.util.Base64;
 import java.util.Map;
+import org.hankki.hankkiserver.common.code.AuthErrorCode;
+import org.hankki.hankkiserver.common.exception.UnauthorizedException;
+import org.springframework.stereotype.Component;
 
 @Component
 public class AppleIdentityTokenParser {
@@ -20,7 +24,8 @@ public class AppleIdentityTokenParser {
         try {
             String encodedHeader = identityToken.split("\\.")[0];
             String decodedHeader = new String(Base64.getUrlDecoder().decode(encodedHeader));
-            return OBJECT_MAPPER.readValue(decodedHeader, Map.class);
+            return OBJECT_MAPPER.readValue(decodedHeader, new TypeReference<>() {
+            });
         } catch (JsonProcessingException | ArrayIndexOutOfBoundsException e) {
             throw new UnauthorizedException(AuthErrorCode.INVALID_APPLE_IDENTITY_TOKEN);
         }

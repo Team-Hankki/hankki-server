ee5f4b7fb4062634f5b65f9fe5724ab008704734
kgy1008 <gayeon1008@g.skku.edu>
2025-01-12 19:09:01 +0900

    [refac] set default user name

--- src/main/java/org/hankki/hankkiserver/external/openfeign/login/dto/SocialInfoDto.java
+++ src/main/java/org/hankki/hankkiserver/external/openfeign/login/dto/SocialInfoDto.java
@@ -3,11 +3,14 @@ package org.hankki.hankkiserver.external.openfeign.login.dto;
 public record SocialInfoDto(
         String serialId,
         String name,
-        String email) {
-    public static SocialInfoDto of(
-            final String serialId,
-            final String name,
-            final String email) {
+        String email)
+{
+    private static final String DEFAULT_NAME = "한끼";
+
+    public static SocialInfoDto of(final String serialId, String name, final String email) {
+        if (name.isBlank()) {
+            name = DEFAULT_NAME;
+        }
         return new SocialInfoDto(serialId, name, email);
     }
 }

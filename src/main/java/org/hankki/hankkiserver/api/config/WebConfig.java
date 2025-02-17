29ec10be33f474d1a37ecece432fdb9996e5e667
Parkjyun <jaeyun0507@naver.com>
2025-02-17 11:15:33 +0900

    [feat] add enum converter

--- src/main/java/org/hankki/hankkiserver/api/config/WebConfig.java
+++ src/main/java/org/hankki/hankkiserver/api/config/WebConfig.java
@@ -1,16 +1,15 @@
 package org.hankki.hankkiserver.api.config;
 
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import org.hankki.hankkiserver.api.common.PriceCategoryConverter;
 import org.hankki.hankkiserver.api.common.SortOptionConverter;
+import org.hankki.hankkiserver.api.common.StoreCategoryConverter;
 import org.hankki.hankkiserver.api.common.UserIdArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
@@ -19,6 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
     private final UserIdArgumentResolver userIdArgumentResolver;
     private final PriceCategoryConverter priceCategoryConverter;
     private final SortOptionConverter sortOptionConverter;
+    private final StoreCategoryConverter storeCategoryConverter;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
@@ -29,5 +29,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(sortOptionConverter);
         registry.addConverter(priceCategoryConverter);
+        registry.addConverter(storeCategoryConverter);
     }
 }
